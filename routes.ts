9c6d6af463823af1b66c1809bf2a92b4c1df2cac
Jerome Doran <disneyfan9184@gmail.com>
2024-09-14 14:14:40 -0400

    added email verification link to registration and login process

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * The routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/'];
+export const publicRoutes = ['/', '/auth/new-verification'];
 
 /**
  * An array of routes that are accessible to the public
